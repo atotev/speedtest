65b978e13bd59f5c9c4bfe1b3161bad5e8daa530
Andrey Totev <a.totev@yahoo.com>
2020-08-26 23:33:32 +0100

    Add Spring Boot application boilerplate

---
+++ test-starter/src/main/java/com/speedtest/svc/starter/SpeedTestStarterServiceApplication.java
@@ -0,0 +1,13 @@
+package com.speedtest.svc.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedTestStarterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeedTestStarterServiceApplication.class, args);
+	}
+
+}
---
+++ test-starter/src/test/java/com/speedtest/svc/starter/SpeedTestStarterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.speedtest.svc.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeedTestStarterServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
